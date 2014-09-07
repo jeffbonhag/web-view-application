9e7f7f4c3705b4a6969e9d256e17bfd7e7091001
Jeff Bonhag <jeff@thebonhags.com>
2014-09-07 11:14:36 -0400

    Keep dots sorted by size (closes #1)

--- app/src/main/assets/src/sketch.js
+++ app/src/main/assets/src/sketch.js
@@ -30,6 +30,15 @@ function draw() {
 	background(255, 255, 255);
 	if (dots.length < maxDots) {
 		dots.push(new Dot());
+		dots.sort(function(a, b) {
+			if (a.diameter < b.diameter) {
+				return -1;
+			} else if (a.diameter == b.diameter) {
+				return 0;
+			} else {
+				return 1;
+			}
+		});
 	}
 
 	var dead = [];
@@ -44,7 +53,6 @@ function draw() {
 	}
 
 	// cleanup dead dots
-	console.log(dead.join(","));
 
 	for (var i in dead) {
 		dots.splice(dead[i], 1);
