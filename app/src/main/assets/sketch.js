3c433382f134c10252489c938a851f72f4f49d31
Jeff Bonhag <jeff@thebonhags.com>
2014-09-07 00:38:15 -0400

    Moving dots

--- app/src/main/assets/sketch.js
+++ app/src/main/assets/sketch.js
@@ -1,17 +1,52 @@
-var x = 0;
+var dots = [];
+var maxDots = 100;
+
+function Dot() {
+	this.diameter = random(100);
+	this.x = random(windowWidth);
+	this.y = this.diameter * -1;
+	this.color = color(
+	      random(255),
+	      random(255),
+	      random(255)
+	);
+}
+
+Dot.prototype.display = function() {
+	noStroke();
+	fill(this.color);
+	ellipse(this.x, this.y, this.diameter, this.diameter);
+}
+
+Dot.prototype.fall = function() {
+	this.y += this.diameter / 50;
+}
 
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  var c = color(
-      random(255),
-      random(255),
-      random(255)
-      );
-  stroke(c);
-  fill(c);
-  ellipse(random(width), random(height), 100, 100);
-  x = x + 1;
+	background(255, 255, 255);
+	if (dots.length < maxDots) {
+		dots.push(new Dot());
+	}
+
+	var dead = [];
+
+	for (var i in dots) {
+		dots[i].display();
+		dots[i].fall();
+
+		if (dots[i].y > windowWidth) {
+			dead.push(i);:w
+		}
+	}
+
+	// cleanup dead dots
+	console.log(dead.join(","));
+
+	for (var i in dead) {
+		dots.splice(dead[i], 1);
+	}
 }
